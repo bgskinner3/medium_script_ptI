41f77daa6172e0d471fd919793a1b05c2da45591
Brennan Skinner <bgskinner3@gmail.com>
2023-02-27 19:37:22 -0500

    changed naming for index

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
-
 const { produce_simple_exchange_paths } = require('./mapping_simple_paths');
 const { get_all_defi_liquidty_pools } = require('./apis');
 
 async function init() {
   try {
-    const x = await get_all_defi_liquidty_pools(1);
+    const defi_array_of_objects = await get_all_defi_liquidty_pools(1);
 
-    if (x) {
-      const y = produce_simple_exchange_paths(x);
-      console.log(y);
+    if (defi_array_of_objects) {
+      const possible_profitable_paths = produce_simple_exchange_paths(x);
+      console.log(possible_profitable_paths);
     }
   } catch (error) {
     console.error(error);
