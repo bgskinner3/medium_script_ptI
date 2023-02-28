0b5e9e47c8f511863553922938518b297a7629f1
Brennan Skinner <bgskinner3@gmail.com>
2023-02-27 19:43:18 -0500

    added constants

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
+/* eslint-disable no-unused-vars */
 const { produce_simple_exchange_paths } = require('./mapping_simple_paths');
 const { get_all_defi_liquidty_pools } = require('./apis');
-
+const {
+  TIME_FRAME_FOR_SUBGRAPH_ONE_HOUR,
+  TIME_FRAME_FOR_SUBGRAPH_FOUR_HOURS,
+  TIME_FRAME_FOR_SUBGRAPH_SIX_HOURS,
+} = require('./constants');
 async function init() {
   try {
-    const defi_array_of_objects = await get_all_defi_liquidty_pools(1);
+    /**
+     * we look for liqudity pools with the highest USD volume and transaction volume from one hour ago.
+     * we can apply different hours from our constants file => 4 HOURS OR 6 HOURS
+     */
+    const defi_array_of_objects = await get_all_defi_liquidty_pools(
+      TIME_FRAME_FOR_SUBGRAPH_ONE_HOUR
+    );
 
     if (defi_array_of_objects) {
-      const possible_profitable_paths = produce_simple_exchange_paths(x);
+      const possible_profitable_paths = produce_simple_exchange_paths(
+        defi_array_of_objects
+      );
       console.log(possible_profitable_paths);
     }
   } catch (error) {
